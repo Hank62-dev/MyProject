6cc6f1faefb943480904da666b3998efb88da71b
Võ Gia Huy <huyvog6226@gmail.com>
2025-06-11 16:03:41 +0700

    Add files via upload

---
+++ MyProfile/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 100 },
+      color: { value: "#fff" },
+      shape: { type: "circle" },
+      opacity: {
+        value: 1,
+        random: true
+      },
+      size: {
+        value: 2,
+        random: true
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#fff",
+        opacity: 0.8,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 7
+      }
+    },
+    interactivity: {
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" }
+      },
+      modes: {
+        repulse: { distance: 100, duration: 0.4 },
+        push: { particles_nb: 4 }
+      }
+    },
+    retina_detect: true
+  });
+});
